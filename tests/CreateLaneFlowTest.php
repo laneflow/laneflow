710d5737008c9719cbb3eda75bd578adc937fcf3
senprints <qtvhao@gmail.com>
2021-03-29 17:18:49 +0700

    add some test for ExampleFlow

--- tests/CreateLaneFlowTest.php
+++ tests/CreateLaneFlowTest.php
@@ -4,6 +4,7 @@
 namespace Laneflow\Laneflow\Tests;
 
 
+use Laneflow\Laneflow\Example\ExampleFlow;
 use Laneflow\Laneflow\LaneFlow;
 use PHPUnit\Framework\TestCase;
 
@@ -11,6 +12,7 @@ class CreateLaneFlowTest extends TestCase
 {
     public function testCanCreateLaneFlow()
     {
+        $this->assertIsObject(new ExampleFlow());
         $this->assertInstanceOf(LaneFlow::class, new LaneFlow());
     }
 }
