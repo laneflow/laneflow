d4ab7e696dd8a254bf048975a59983e12254671c
senprints <qtvhao@gmail.com>
2021-03-30 23:20:03 +0700

    connect SwimLane with Lane

--- src/SwimLane/Lanes.php
+++ src/SwimLane/Lanes.php
@@ -9,4 +9,22 @@ use Laneflow\Laneflow\Collection;
 class Lanes extends Collection
 {
 
+    protected SwimLane $swimlane;
+    /**
+     * @param SwimLane $swimlane
+     * @return Lanes
+     */
+    public function setSwimlane(SwimLane $swimlane): Lanes
+    {
+        $this->swimlane = $swimlane;
+        return $this;
+    }
+
+    /**
+     * @return SwimLane
+     */
+    public function getSwimlane(): SwimLane
+    {
+        return $this->swimlane;
+    }
 }
