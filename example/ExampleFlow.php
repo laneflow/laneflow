de7c64491b1e35aa55adb8a8ccbcb6bab3b4bdf0
senprints <qtvhao@gmail.com>
2021-03-31 05:04:17 +0700

    add Process for ExampleFlow

--- example/ExampleFlow.php
+++ example/ExampleFlow.php
@@ -5,7 +5,10 @@ namespace Laneflow\Laneflow\Example;
 
 
 use Laneflow\Laneflow\Example\Lanes\CustomerLane;
+use Laneflow\Laneflow\Example\Lanes\SalesLane;
 use Laneflow\Laneflow\Example\Process\CustomerSubmitsPurchaseOrder;
+use Laneflow\Laneflow\Example\Process\EntersOrder;
+use Laneflow\Laneflow\Example\Process\RepLogsPO;
 use Laneflow\Laneflow\Example\Step\StepCreateOrder;
 use Laneflow\Laneflow\LaneFlow;
 
@@ -22,20 +25,30 @@ class ExampleFlow extends LaneFlow
         parent::__construct();
         //PROCESSES
         $customerSubmitsPurchaseOrder = new CustomerSubmitsPurchaseOrder();
+        $repLogsPO = new RepLogsPO();
+        $entersOrder = new EntersOrder();
 
         //LANES
         $customerLane = new CustomerLane();
         $customerLane
             ->addProcess($customerSubmitsPurchaseOrder)
         ;
+        $salesLane = new SalesLane();
+        $salesLane
+            ->addProcess($repLogsPO)
+            ->addProcess($entersOrder)
+        ;
 
         //STEPS
         $stepCreateOrder = new StepCreateOrder();
         $customerSubmitsPurchaseOrder->setStep($stepCreateOrder);
+        $repLogsPO->setStep($stepCreateOrder);
+        $entersOrder->setStep($stepCreateOrder);
 
         $this
             ->getSwimLane()
             ->addLane($customerLane)
+            ->addLane($salesLane)
             ->addStep($stepCreateOrder)
         ;
     }
---
+++ example/Process/RepLogsPO.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Laneflow\Laneflow\Example\Process;
+
+
+use Laneflow\Laneflow\SwimLane\Process\Process;
+
+class RepLogsPO extends Process
+{
+
+}
