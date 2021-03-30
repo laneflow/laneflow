2b0f9d45ba53dd37fe6362298a9e48ca9892ca98
senprints <qtvhao@gmail.com>
2021-03-31 02:09:41 +0700

    add code for Step

--- src/SwimLane/Step/Step.php
+++ src/SwimLane/Step/Step.php
@@ -6,5 +6,23 @@ namespace Laneflow\Laneflow\SwimLane\Step;
 
 class Step
 {
+    protected string $code;
 
+    /**
+     * @param string $code
+     * @return Step
+     */
+    public function setCode(string $code): Step
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
 }
