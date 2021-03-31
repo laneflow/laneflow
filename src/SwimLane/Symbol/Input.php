423a35e9f90fc8b21f0a13d7bb1ce088e6532698
senprints <qtvhao@gmail.com>
2021-04-01 02:37:28 +0700

    add Symbol Input/Output

--- src/SwimLane/Symbol/Input.php
+++ src/SwimLane/Symbol/Input.php
@@ -4,7 +4,17 @@
 namespace Laneflow\Laneflow\SwimLane\Symbol;
 
 
+use JetBrains\PhpStorm\Pure;
+
 class Input extends Symbol
 {
+    #[Pure] public function __toString(): string
+    {
+        $label = $this->getLabel();
+
+        return "<div style='  justify-content: center;  display: flex;    align-items: center;min-height: 28px;text-align:center;font-size: 12px; border: 1px solid black; padding: 6px 10px;  margin: 10px;	width: 100px;
+	height: 50px;
+	transform: skew(-10deg);'>$label</div>";
+    }
 
 }
